0c5d475e3190e49f361d772bd9b96e5f17e43da1
lowkislucas <lucasw1436@gmail.com>
2024-01-03 20:53:19 -0500

    metal spheres

--- color.h
+++ color.h
@@ -7,6 +7,10 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    return sqrt(linear_component);
+}
+
 void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     double r = pixel_color.x();
     double g = pixel_color.y();
@@ -15,6 +19,11 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     r /= (double) samples_per_pixel;
     g /= (double) samples_per_pixel;
     b /= (double) samples_per_pixel;
+
+    // Applying gamma transform
+    //r = linear_to_gamma(r);
+    //g = linear_to_gamma(g);
+    //b = linear_to_gamma(b);
 
     // Write the translated [0,255] value of each color component.
     static const interval intensity(0.000, 0.999);
